0de9049e63644ea5c26ea1f6999369359ac27fd5
Victor Loginov <tkoe88@zoho.com>
2023-08-14 14:57:46 +0200

    challenge stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,17 @@
 # This class handles CRUD operations for managing questions.
 class QuestionsController < ApplicationController
   def about; end
+
+  def ask; end
+
+  def answer
+    question = params[:answer]
+    if question.end_with?('?')
+      @answer = 'Stupid question, get back to work'
+    elsif question.include?('I am going to work right now')
+      @answer = 'Good, piss off to work'
+    else
+      @answer = 'I don\'t care, get dressed and go to work'
+    end
+  end
 end
