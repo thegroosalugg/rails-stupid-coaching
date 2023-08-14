55c3633c933d7b8af155bfc826f796071b3df9f6
Victor Loginov <tkoe88@zoho.com>
2023-08-14 11:17:02 +0200

    base set-up

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# This class handles CRUD operations for managing questions.
+class QuestionsController < ApplicationController
+  def about; end
+end
---
+++ app/helpers/questions_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# This module handles CRUD operations for managing questions.
+module QuestionsHelper
+end
